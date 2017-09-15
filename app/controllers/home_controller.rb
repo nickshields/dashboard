dd98c8d05eba9b7cfb2edc5d4ac1b8bc17da812c
nickshields <nwshield@uwaterloo.ca>
2017-09-15 16:01:32 -0400

    Implemented login functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+   before_action :authorize
    def show
      @devices = Device.all
    	 @connection = Connection.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'device/list'
-  root 'home#show'
+  #root 'home#show'
 
    get 'home/show'
    get 'device/new'
@@ -14,4 +16,27 @@ Rails.application.routes.draw do
    get 'device/update'
    get 'device/show_subjects'
    get 'device/refresh'
+
+
+   # This route sends requests to our naked url to the *cool* action in the *gif* controller.
+     root to: 'home#show'
+
+  # I've created a gif controller so I have a page I can secure later.
+  # This is optional (as is the root to: above).
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'users#login'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+
+
+
+
 end
